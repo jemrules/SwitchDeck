9c49fe1e78c0e6a3faf5eb4011e555e91e93b135
jemrules <evern.mccullough@comcast.net>
2025-06-23 11:49:29 -0600

    Update switch_connection_handler.py

--- switch_connection_handler.py
+++ switch_connection_handler.py
@@ -27,6 +27,7 @@ class test:
             print("Thread function completed")
         self.init_variables()
         self.event_queue = asyncio.Queue() #type (EventType, args:list[any])
+        self.send_input = False
         self._thread = threading.Thread(target=thread_function)
         self._thread.daemon = True
         self._thread.start()
@@ -39,18 +40,48 @@ class test:
     def event_connect_device(self, address=None):
         print(f"Connecting to device at {address}")
         self.event_queue.put_nowait((EventType.CONNECT_DEVICE, [address]))
+    def event_disconnect_device(self):
+        print("Disconnecting device")
+        self.event_queue.put_nowait((EventType.DISCONNECT_DEVICE, []))
+    
+    def button_press(self, button_name):
+        if self.controller_state:
+            self.controller_state.button_state.set_button(button_name, True)
+            self.send_input = True
+        else:
+            print("Controller is not connected")
+    def button_release(self, button_name):
+        if self.controller_state:
+            self.controller_state.button_state.set_button(button_name, False)
+            self.send_input = True
+        else:
+            print("Controller is not connected")
+    def move_stick(self,stick="l",direction="x",scale=1):
+        if not self.controller_state:
+            print("Controller is not connected")
+            return
+        scale=min((scale+1)/2*(0x1000),0x1000-1)
+        a=None
+        match stick.lower()[0]:
+            case "l":
+                a=self.controller_state.l_stick_state
+            case "r":
+                a=self.controller_state.r_stick_state
+        match direction.lower()[0]:
+            case "x":
+                a.set_h(scale)
+            case "y":
+                a.set_v(scale)
+
     async def connect_device(self, address=None):
         with utils.get_output(default=None) as capture_file:
             controller = Controller.PRO_CONTROLLER
-            spi_flash = FlashMemory()
-            factory = controller_protocol_factory(controller,spi_flash=spi_flash)
+            self.spi_flash = FlashMemory()
+            self.factory = controller_protocol_factory(controller,spi_flash=self.spi_flash)
             ctl_psm, itr_psm = 17, 19
-            transport, protocol = await create_hid_server(factory,
-                                                        reconnect_bt_addr=address,
-                                                        ctl_psm=ctl_psm,
-                                                        itr_psm=itr_psm)
-            controller_state = protocol.get_controller_state()
-        await controller_state.connect()
+            self.transport, self.protocol = await create_hid_server(self.factory,reconnect_bt_addr=address,ctl_psm=ctl_psm,itr_psm=itr_psm)
+            self.controller_state = self.protocol.get_controller_state()
+        await self.controller_state.connect()
         print(f"Connected to device at {address}")
         self.event_queue.task_done()
     async def run(self):
@@ -66,11 +97,16 @@ class test:
                     await self.connect_device(address)
                 case EventType.DISCONNECT_DEVICE:
                     ...
+            if self.controller_state and self.send_input:
+                if self.controller_state.is_connected():
+                    print(f"Controller state: {self.controller_state}")
+                else:
+                    print("Controller is not connected")
 
 if __name__ == "__main__":
     print("Finished")
     t = test()
-    t.event_connect_device()
+    t.event_connect_device("60:6B:FF:9B:65:C9")
     while True:
         ...
 
