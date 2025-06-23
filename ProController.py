0ae895cc9df84bff339623cc63d3910d5a4ef667
jemrules <evern.mccullough@comcast.net>
2025-06-23 13:24:12 -0600

    Update ProController.py

--- ProController.py
+++ ProController.py
@@ -118,7 +118,7 @@ class InputHandler(SteamDeckController):
             return
         print("Handling input...")
         for key, value in self.JOYCON_DIGITAL_KEYS.items():
-            # print(f"Key: {key}, Value: {value}, Digital Value: {digital[key]}")
+            print(f"Key: {key}, Value: {value}, Digital Value: {digital[key]}")
             if digital[key]>0.5:
                 self.SwitchHandler.button_press(value)
             else:
@@ -223,7 +223,7 @@ class GUI(QMainWindow):
         sys.exit(0)
         return super().close()
     def update_connection_menu(self):
-        print("Updating connection menu...")
+        # print("Updating connection menu...")
         match self.SwitchHandler.status:
             case ConnectionStatus.CONNECTED:
                 self.disconnect_action.setEnabled(True)
